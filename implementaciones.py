86bb6f7a01ed5f5bb596968515a620ba19ee4e91
Leonel R <leorhcp2030@gmail.com>
2017-10-29 03:11:41 -0300

    Update implementaciones.py

--- implementaciones.py
+++ implementaciones.py
@@ -1,3 +1,35 @@
-def matener_condicion(n):
-  
+def matener_condicion_0_255(numero):
+	""" mantiene al numero entre 0 y 255
+    pre: se asume que numero es entero
+  	"""
+  	return n%256 if n>0 else 256-n
+
+#implementacion para la cola
+def incrementar_bite(self):
+	valor_nuevo = self.prim.dato + 1
+	self.prim.dato = matener_condicion_0_255(valor_nuevo)
+
+def decrementar_bite(self)
+	valor_nuevo = self.prim.dato - 1
+	self.prim.dato = matener_condicion_0_255(valor_nuevo)
+
+def desencolar_encolar_bites(self):
+	if not self.prim == self.ultimo:
+		return
+	
+	self.prim.dato, self.ultimo.dato =  self.ultimo.dato, self.prim.dato
+
+
+Comando = {"!": cola.encolar(0),
+		   "=": cola.desencolar_encolar_bites(),
+		   "-": cola.decremantar_bite(),
+		   "_": cola.incrementar_bite(),
+		   "/": NOSE, #seguramente una funcion q activa la pila
+		   "\": NOSE, No agarra este caracter preguntar
+		   "*":
+			}	
+#Package de errores personalizados
+class SintaxisError(Exception):
+	pass
+
   
